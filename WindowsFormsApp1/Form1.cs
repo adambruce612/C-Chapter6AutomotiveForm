4d8e49c0a192e9b9a6925e12f7b566a7dc982aa0
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-12-12 19:32:31 -0600

    Add files via upload

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -49,16 +49,97 @@ namespace WindowsFormsApp1
             }
 
             return charges;
-        } 
+        }
+
+        private double MiscCharges()
+        {
+            double charges = 0;
+
+            if (checkBoxInspection.Checked)
+            {
+                charges += 15.00;
+            }
+
+            if (checkBoxReplaceMuffler.Checked)
+            {
+                charges += 100.00;
+            }
+
+            if (checkBoxTireRotation.Checked)
+            {
+                charges += 20.00;
+            }
+
+            return charges;
+        }
+
+        private double TaxCharges()
+        {
+            double charges = 0;
+
+            if (textBoxParts.Text != "")
+            {
+               
+            }
+
+            return charges;
+        }
         private void buttonCalculate_Click(object sender, EventArgs e)
         {
-            double totalFees = OilLubeCharges() + FlushCharges();
+            double partsCost;
+            double laborCost;
 
-            labelTotalFeesDisplay.Text = totalFees.ToString();
+            if (double.TryParse(textBoxParts.Text, out partsCost) && double.TryParse(textBoxLabor.Text, out laborCost) )
+            {
+                double totalFees = OilLubeCharges() + FlushCharges() + MiscCharges();
+
+                labelTotalFeesDisplay.Text = totalFees.ToString();
+            }
+            else
+            {
+                
+            }
+        }
+
+        private void ClearOilLube()
+        {
+            checkBoxOilChange.Checked = false;
+            checkBoxLubeJob.Checked = false;
+        }
+
+        private void ClearFlushes()
+        {
+            checkBoxRadiatorFlush.Checked = false;
+            checkBoxTransmissionFlush.Checked = false;
+        }
+
+        private void ClearMisc()
+        {
+            checkBoxInspection.Checked = false;
+            checkBoxReplaceMuffler.Checked = false;
+            checkBoxTireRotation.Checked = false;
+        }
+
+        private void ClearOther()
+        {
+            textBoxParts.Text = "";
+            textBoxLabor.Text = "";
+        }
+
+        private void ClearFees()
+        {
+            labelServiceAndLaborDisplay.Text = "";
+            labelPartsDisplay.Text = "";
+            labelTaxDisplay.Text = "";
+            labelTotalFeesDisplay.Text = "";
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
+            ClearOilLube();
+            ClearFlushes();
+            ClearMisc();
+            ClearOther();
 
         }
 
